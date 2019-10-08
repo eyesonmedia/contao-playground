39950bc8efcbf686f9bf23cc909188a98bb1fea3
Burak Aydin <aydin@eonm.de>
2019-10-08 14:56:40 +0200

    sendmail fix

--- src/Service/Search.php
+++ src/Service/Search.php
@@ -360,7 +360,7 @@ class Search
          */
         $transport = \Swift_SmtpTransport::newInstance( 'w00a0565.kasserver.com', 587 );
         $transport->setUsername( 'm042205d' );
-        $transport->setPassword( 'NbdKxAATzYw2QL8N' );
+        $transport->setPassword( 'U6pHoCadsyGCAWd8' );
 
         /*
          * get message class / set message and set mail header
--- src/SiowebDummyBundle.php
+++ src/SiowebDummyBundle.php
@@ -232,7 +232,7 @@ class SiowebDummyBundle extends Bundle
          */
         $transport = \Swift_SmtpTransport::newInstance( 'w00a0565.kasserver.com', 587 );
         $transport->setUsername( 'm042205d' );
-        $transport->setPassword( 'NbdKxAATzYw2QL8N' );
+        $transport->setPassword( 'U6pHoCadsyGCAWd8' );
 
         /*
          * get message class / set message and set mail header
@@ -292,7 +292,7 @@ class SiowebDummyBundle extends Bundle
          */
         $transport = \Swift_SmtpTransport::newInstance( 'w00a0565.kasserver.com', 587 );
         $transport->setUsername( 'm042205d' );
-        $transport->setPassword( 'NbdKxAATzYw2QL8N' );
+        $transport->setPassword( 'U6pHoCadsyGCAWd8' );
 
         /*
          * get message class / set message and set mail header
